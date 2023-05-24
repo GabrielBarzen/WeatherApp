91463e29cca160d9a5a679d0652c25c2708deead
Anton <anton.holm.y@gmail.com>
2023-05-24 12:21:44 +0200

    add basic footer

--- WeatherGame/src/App.vue
+++ WeatherGame/src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-  import Navigation from "./components/Navigation.vue"
+    import Navigation from "./components/Navigation.vue"
+    import Footer from "./components/Footer.vue"
 </script>
 
 <template>
-  <Navigation />
-  <main>
-    <p>Vue is running</p>
-  </main>
-  <router-view />
+    <Navigation />
+    <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <p>Vue is running</p>
+        <router-view />
+    </main>
+    <Footer class="absolute inset-x-0 bottom-0 h-16" />
 </template>
 
 <style scoped>
---
+++ WeatherGame/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <footer class="bg-gray-800">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <p class="text-white text-center">Footer</p>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+</style>
