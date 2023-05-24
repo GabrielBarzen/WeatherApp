ba5a4a43fac7577e8b44207c0ae50fbab8fbde38
Gabriel Modin Bärzén <gabriel.barzen@mailfence.com>
2023-05-24 20:09:36 +0200

    Added ApiTicker to App.vue. Added the ticker checking for updates to App.vue.

--- WeatherGame/src/App.vue
+++ WeatherGame/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
     import Navigation from "./components/Navigation.vue"
     import Footer from "./components/Footer.vue"
+    import ApiTicker from "./components/ApiTicker.vue"
 </script>
 
 <template>
@@ -8,6 +9,7 @@
     <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <p>Vue is running</p>
         <router-view />
+        <ApiTicker/>
     </main>
     <Footer class="absolute inset-x-0 bottom-0 h-16" />
 </template>
