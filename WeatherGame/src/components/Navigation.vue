cd31212826d6f6db4a070cf0829ca9ba368fffa5
Anton <anton.holm.y@gmail.com>
2023-05-25 21:42:21 +0200

    hide tracking button in navbar on smaller displays

--- WeatherGame/src/components/Navigation.vue
+++ WeatherGame/src/components/Navigation.vue
@@ -37,7 +37,7 @@
                     </div>
                 </div>
                 <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <ApiTicker/>
+                    <ApiTicker class="hidden md:block"/>
                     <router-link type="button" to="/settings" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none">
                         <Cog6ToothIcon class="h-6 w-6" aria-hidden="true"/>
                     </router-link>
