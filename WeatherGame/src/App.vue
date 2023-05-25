96da739ceaf7fc98b838fcd37ed5cfab78d80e09
Anton <anton.holm.y@gmail.com>
2023-05-25 21:31:51 +0200

    remove troubleshooting text

--- WeatherGame/src/App.vue
+++ WeatherGame/src/App.vue
@@ -6,7 +6,6 @@
 <template>
     <Navigation />
     <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <p>Vue is running</p>
         <router-view />
     </main>
     <Footer class="absolute inset-x-0 bottom-0 h-16" />
