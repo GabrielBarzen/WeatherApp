a3e7f6761bc8decb158f03c8dbc6d95f15e9881c
Anton <anton.holm.y@gmail.com>
2023-05-28 22:25:58 +0200

    show popup when clicking on delete data

--- WeatherGame/src/components/DeleteButton.vue
+++ WeatherGame/src/components/DeleteButton.vue
@@ -7,7 +7,7 @@
 </script>
 
 <template>
-    <a @click="deleteData" class="inline-flex items-center cursor-pointer rounded border-2 border-red-600 bg-transparent px-8 py-3 text-sm font-medium text-red-600 hover:text-white transition-colors hover:bg-red-600 hover:border-red-500 active:opacity-75">{{ props.buttonText }}</a>
+    <a class="inline-flex items-center cursor-pointer rounded border-2 border-red-600 bg-transparent px-8 py-3 text-sm font-medium text-red-600 hover:text-white transition-colors hover:bg-red-600 hover:border-red-500 active:opacity-75">{{ props.buttonText }}</a>
 </template>
 
 <style scoped>
--- WeatherGame/src/views/Settings.vue
+++ WeatherGame/src/views/Settings.vue
@@ -1,10 +1,55 @@
 <script setup>
+    import { ref } from "vue"
     import Toggle from "../components/Toggle.vue"
     import DeleteButton from "../components/DeleteButton.vue"
+    import DeleteWarning from "../components/DeleteWarning.vue"
+
+    let deletePopup = ref(false);
+
+    function pressButton () {
+        deletePopup.value = !deletePopup.value; //should set to true, and button inside popup will set to false
+    }
+
+    function confirmButton () {
+        //clear local storage
+        pressButton();
+    }
+
+
 </script>
 
 <template>
     <div class="md:max-w-7xl mx-auto">
+        <!-- <DeleteWarning v-if="deletePopup" class="fixed top-44 left" /> -->
+        <div v-if="deletePopup" class="rounded-lg bg-white p-8 shadow-2xl">
+        <h2 class="text-lg font-bold">Are you sure you want to do that?</h2>
+    
+        <p class="mt-2 text-sm text-gray-500">
+        Doing that could have cause some issues elsewhere, are you 100% sure it's
+        OK?
+        </p>
+    
+        <div class="mt-4 flex gap-2">
+        <button @click="confirmButton"
+            type="button"
+            class="rounded bg-green-50 px-4 py-2 text-sm font-medium text-green-600"
+        >
+            Yes, I'm sure
+        </button>
+    
+        <button @click="pressButton"
+            type="button"
+            class="rounded bg-gray-50 px-4 py-2 text-sm font-medium text-gray-600"
+        >
+            No, go back
+        </button>
+        </div>
+    </div>
+
+
+
+
+
         <h1 class="text-2xl p-4 m-1">Settings</h1>
             <div id="settings-container" class="">
                 <ul class="p-4 m-1 text-lg">
@@ -12,14 +57,9 @@
                         <span class="max-sm:mb-2">Temperature Unit</span>
                         <Toggle val1="C" val2="F" name="tempUnit" />
                     </li>
-
-                    <li class="flex justify-between items-center bg-secondary-background p-4 my-4 rounded max-sm:flex-col">
-                        <span class="max-sm:mb-2">Temperature Unit</span>
-                        <Toggle val1="On" val2="Off" name="test" />
-                    </li>
                 </ul>
                 <div class="flex justify-center mt-8">
-                    <DeleteButton buttonText="Delete all my data" class="" />
+                    <DeleteButton v-on:click="pressButton" buttonText="Delete all my data" class="" />
                 </div>
             </div>
     </div>
