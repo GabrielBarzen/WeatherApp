b1529bdd05c5b639b06ecfc1ddf234165d712a5a
Anton <anton.holm.y@gmail.com>
2023-05-28 21:32:16 +0200

    add delete button component

---
+++ WeatherGame/src/components/DeleteButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const props = defineProps(["buttonText"])
+ 
+    function deleteData() {
+        alert("lol")
+    }
+</script>
+
+<template>
+    <a @click="deleteData" class="inline-flex items-center cursor-pointer rounded border-2 border-red-600 bg-transparent px-8 py-3 text-sm font-medium text-red-600 hover:text-white transition-colors hover:bg-red-600 hover:border-red-500 active:opacity-75">{{ props.buttonText }}</a>
+</template>
+
+<style scoped>
+</style>
