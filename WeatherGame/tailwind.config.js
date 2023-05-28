82d9cd26463b95de8463470666f63365b18a53e1
Anton <anton.holm.y@gmail.com>
2023-05-28 18:08:53 +0200

    add color to tailwind config

--- WeatherGame/tailwind.config.js
+++ WeatherGame/tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './public/**/*.html',
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    extend: { 
+      colors: {
+      "primary-background": "#f4f4f5",
+      "secondary-background": "#cbd5e1",
+      "text-color": "#3f3f46",
+      "text-highlight": "#0ea5e9",
+      "background-highlight": "#e4e4e7"
+      }
+    },
   },
   plugins: [],
 }
