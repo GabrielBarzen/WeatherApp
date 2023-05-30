fb5cb7eff8393a6d3740a307678b7a29414db49f
Gabriel Modin Bärzén <gabriel.barzen@mailfence.com>
2023-05-30 12:11:53 +0200

    Changed styling for achievements to be better on mobile

--- WeatherGame/src/components/AchievementPopup.vue
+++ WeatherGame/src/components/AchievementPopup.vue
@@ -5,7 +5,7 @@
 <template>
 <div v-if="show" class="" >
     <a href="/achievements"
-      class="fixed left-1/3 right-1/3 bottom-64 mx-auto block rounded-xl border border-gray-800 bg-secondary-background p-4 shadow-xl sm:p-6 lg:p-8 text-center" 
+      class="fixed left-1/4 right-1/4 bottom-64 mx-auto block rounded-xl border border-gray-800 bg-secondary-background p-4 shadow-xl sm:p-6 lg:p-8 text-center" 
     >
     <img :src="image" :alt="altText" class="mx-auto object-cover h-32">
       <h3 class="mt-3 text-lg font-bold text-text-color sm:text-xl">
